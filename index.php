1718b70b78330ddbef52591f071e3761f32f5abc
DomenicoCicero <domenico.cicero8@gmail.com>
2024-05-03 14:23:44 +0200

    finish login

--- index.php
+++ index.php
@@ -40,15 +40,15 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     if($user_from_db) {
         if(password_verify($_POST['password'], $user_from_db['password'])) {
             $_SESSION["user_id"] = $user_from_db['user_id'];
-            echo $_SESSION;
+            // echo $_SESSION;
+            echo('Ciao ' . $user_from_db["username"]);
         };
     }
 
-    
+    $error['credentials'] = "Credenziali non valide";
 
 
 }
-
 
 ?>
 
@@ -57,7 +57,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
 </head>
