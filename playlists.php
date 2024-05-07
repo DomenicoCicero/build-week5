6175b23b2b0e37c816a50bfe63ae58151ebe8bc8
marco9727 <marco.albarella97@gmail.com>
2024-05-07 14:37:20 +0200

    commit href

--- playlists.php
+++ playlists.php
@@ -223,7 +223,7 @@ if(isset($_SESSION['user_id'])) {
       <div class="container-fluid px-4">
         <div class="d-flex justify-content-between">
         <h4><?= $playlist['name'] ?></h4>
-        <a class='btn btn-danger' href=''>Elimina Playlist</a>
+        <a class='btn btn-danger' href="http://localhost/progetto-netflix-php/build-week5/deletePlaylist.php?playlistId=".urlencode($playlistId)>Elimina Playlist</a>
         </div>
         <div
           class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4"
