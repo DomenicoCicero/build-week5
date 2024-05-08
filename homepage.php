dadecc1e57ccfd2d56f3129b98a873ae0aef85de
marco9727 <marco.albarella97@gmail.com>
2024-05-08 11:15:34 +0200

    commit

--- homepage.php
+++ homepage.php
@@ -25,7 +25,7 @@ if(isset($_SESSION['user_id'])) {
   $stmt = $pdo->prepare('SELECT * FROM movies WHERE title LIKE ? AND genre LIKE ?');
   $stmt -> execute([
     "%$search%",
-    "%$search%",
+    // "%$search%",
     "%$selectedGenre%"
   ]);
 
