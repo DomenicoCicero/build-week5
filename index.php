c0327c44ec0e384e91d1860bfaf2b1fabc3444ac
marco9727 <marco.albarella97@gmail.com>
2024-05-08 12:50:16 +0200

    commit

--- index.php
+++ index.php
@@ -69,14 +69,14 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 ?>
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
     <link rel="stylesheet" href="login-registrazione.css"/>
 </head>
 <body>
@@ -88,7 +88,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     <form action="" method="POST" novalidate  >
         <div class="form-control">
             <label for="username" >Username</label>
-            <input type="text" class="form-control" id="username" name="username" value="<?= $user['username'] ?>">
+            <input type="text" class="form-control" id="username" name="username" value="">
         </div>
         <div class="form-control">
             <label for="password">Password</label>
@@ -107,4 +107,68 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     <a href="http://localhost/progetto-netflix-php/build-week5/registrazione.php">Vai alla registrazione</a>
  </div>
 </body>
+</html> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+<link rel="stylesheet" href="login-registrazione.css">
+<title>Epiflix</title>
+</head>
+<body>
+    <header class="showcase">
+        
+
+            <div class="logo">
+                <img src="https://i.ibb.co/r5krrdz/logo.png">
+            </div>
+
+            <div class="showcase-content">
+                <div class="formm">
+                    <form action="" method="POST" novalidate >
+                        <h1>Login</h1>
+                        <div class="info">
+                            <!-- <input class="email" type="email" placeholder="Email or phone number"> <br> -->
+                            <input type="text" class="username"  name="username" placeholder="username" value="<?= $user['username'] ?>">
+                            <!-- <input class="email" type="password" placeholder="Password"> -->
+                            <input type="password" class="username" placeholder="password" name="password" value="">
+                        </div>
+                        <div class="btn">
+                            <button class="btn-primary" type="submit">Login</button>
+                        </div>
+                        <div class="help">
+                            <div>
+                                <input value="true" type="checkbox"><label>Ricordami</label>
+                            </div>
+
+                            <a href="https://www.netflix.com/dz-en/LoginHelp">Need Help ?</a>
+                        
+                        </div>
+
+                    </form>
+    
+                </div>
+                
+                
+                <div class="signup">
+                    <p>New to Netflix ?</p>
+                    <a href="http://localhost/progetto-netflix-php/build-week5/registrazione.php">Registrati</a>
+                </div>
+                
+
+
+            </div>
+
+       
+            
+       
+    </header>
+
+
+</body>
 </html>
+
+
+
