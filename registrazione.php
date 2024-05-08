73612a2dbdc0e20907904a28539834cd8bbbca9d
DomenicoCicero <domenico.cicero8@gmail.com>
2024-05-08 14:27:55 +0200

    prova alert

--- registrazione.php
+++ registrazione.php
@@ -49,10 +49,14 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
     if ($success) {
-        $alertMessage = '<div class="alert alert-success" role="alert">
-                            <h4 class="alert-heading">Registrazione completata!</h4>
-                            <p>Benvenuto a Netflix Clone.</p>
-                        </div>';
+        $alertMessage = '<div style="padding: 1rem; margin-bottom: 1rem; border: 1px solid transparent; border-radius: 0.25rem; color: #155724; background-color: #d4edda; border-color: #c3e6cb;" role="alert">
+        <h4 style="margin-top: 0; margin-bottom: 0.5rem;">Registrazione completata!</h4>
+        <p style="margin-top: 0; margin-bottom: 0;">Benvenuto a Netflix Clone.</p>
+    </div>';
+        // $alertMessage = '<div class="alert alert-success" role="alert">
+        //                     <h4 class="alert-heading">Registrazione completata!</h4>
+        //                     <p>Benvenuto a Netflix Clone.</p>
+        //                 </div>';
             
     } else {
         $alertMessage = '<div class="alert alert-danger" role="alert">
@@ -71,40 +75,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 ?>
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
-    <link rel="stylesheet" href="style.css"/>
-</head>
-<body>
-
-    <div class="  ">
-    <h1>Registrazione</h1>
-    <form action="" method="POST" novalidate  class="mb-4">
-        <div class="mb-3">
-            <label for="username" class="form-label">Username</label>
-            <input type="text" class="form-control" id="username" name="username" value=">">
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">email</label>
-            <input type="email" class="form-control" id="email" name="email" value=">">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password" value=">">
-        </div>
-        <button type="submit" class="btn btn-primary">Registrati</button>
-    </form>
-    
- </div>
-</body>
-</html> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -129,14 +99,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <h1>Registrazione</h1>
                         <div class="<?= $success ? 'd-none' : 'container'?>  ">
                         <div class="info">
-                            <!-- <input class="email" type="email" placeholder="Email or phone number"> <br> -->
                             <input type="text" class="email"  name="username" placeholder="username" required value="<?= $user['username'] ?>">
-                            <!-- <input class="email" type="password" placeholder="Password"> -->
                             <input type="email" class="email" placeholder="email"  name="email" required value="<?= $user['email'] ?>">
                             <input type="password" class="email" placeholder="password" name="password" required value="<?= $user['password'] ?>">
                         </div>
                         <div class="btn">
-                            <button class="btn-primary" type="submit">Login</button>
+                            <button class="btn-primary" type="submit">Registrati</button>
                         </div>
                         <div class="help">
                             <div>
