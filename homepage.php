25a6cd34ed71138a9aa362cfe88021dd546139b9
DomenicoCicero <domenico.cicero8@gmail.com>
2024-05-08 14:58:47 +0200

    ms

--- homepage.php
+++ homepage.php
@@ -151,9 +151,9 @@ if(isset($_POST['add-playlist'])) {
       </nav>
       <div class="container-fluid px-4">
         <div class="d-flex justify-content-between">
-          <div class="d-flex">
+          <div class="d-flex align-items-center">
             <h2 class="mb-4">TV Shows</h2>
-            <div class="btn-group" role="group">
+            <div class="btn-group ms-4" role="group">
               <form method="post">
               <select name="genre" id="genre" style="background-color: #221f1f" class="text-white py-1">
               <option value="<?= $selectedGenre ?>"><?= $selectedGenre === "" ? "Tutti i Generi" : $selectedGenre ?></option>
